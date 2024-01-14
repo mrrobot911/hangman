2eae148a718516c7b8f9c2f0a1de8a90d9f9e668
mrrobot911 <kelwin2705@gmail.com>
2024-01-14 23:03:11 +0300

    feat: reset svg

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+let svgDoc;
+
 const body = document.querySelector('body');
 const scaffold = document.createElement('div');
 scaffold.className = 'scaffold-container';
-const svg = document.createElement('div');
-svg.className = 'svg-el';
+const svg = document.createElement('object');
+svg.className = 'image-el';
+svg.data = './assets/human.svg';
+svg.type = 'image/svg+xml';
+function resetSvg() {
+  if (svgDoc) {
+    [1, 2, 3, 4, 5, 6].forEach((el) => {
+      svgDoc.querySelectorAll(`.element${el}`).forEach((el2) => {
+        el2.setAttribute('visibility', 'hidden');
+      });
+    });
+  }
+}
+function initializeSvg() {
+  svgDoc = svg.contentDocument;
+  resetSvg();
+}
+svg.addEventListener('load', initializeSvg);
+
 const hero = document.createElement('h1');
 hero.className = 'hero-el';
 hero.innerText = 'Hangman game'.toUpperCase();
